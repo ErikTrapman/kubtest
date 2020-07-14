faaae8d9963ced1b93eeae568b2e6f79f7c0906b
erik-zicht <erik@zicht.nl>
2020-07-14 12:55:22 +0200

    Upd splash

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-echo "1<br>";
-echo "2<br>";
-echo "3<br>";
-echo "Are you not entertained!<br>";
-die;
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>K8S</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="mb-5"></div>
+    <div class="jumbotron">
+        <h1 class="display-4">Are you not entertained?!</h1>
+        <p class="lead">.</p>
+        <hr class="my-4">
+        <!--        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>-->
+    </div>
+
+    <pre>
+        <?php
+        print_r($_SERVER);
+        ?>
+    </pre>
+</div>
+</body>
+</html>
